da8fa4cbaba62ae270e3397f97aba6c59a7e53ea
tomrez12345 <126824722+tomrez12345@users.noreply.github.com>
2023-03-06 15:59:17 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,9 +27,8 @@ streamlit.header("Fruityvice Fruit Advice!")
 
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
 
-# prida listu 
+# normalizeace 
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # pridani tabulky
 streamlit.dataframe(fruityvice_normalized)
