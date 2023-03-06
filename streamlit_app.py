19ce7b506f259197e8c6b107fe66a2c918b31a3d
tomrez12345 <126824722+tomrez12345@users.noreply.github.com>
2023-03-06 15:57:17 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,3 +29,5 @@ import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response.json())
 
+# predelani do tabulky
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
