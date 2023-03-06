1a065175d09d1a124e5fc3491a3f1f23659f5fa6
tomrez12345 <126824722+tomrez12345@users.noreply.github.com>
2023-03-06 17:26:36 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,7 @@
 import streamlit
+import pandas
+import snowflake.connector
+from urllib.error import URLError
 
 streamlit.title ('My Parents New Healthy Diner')
 
@@ -10,7 +13,7 @@ streamlit.text ('🥑🍞 Avocado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
-import pandas 
+ 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
@@ -37,7 +40,7 @@ streamlit.dataframe(fruityvice_normalized)
 
 streamlit.stop ()
 
-import snowflake.connector
+
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
