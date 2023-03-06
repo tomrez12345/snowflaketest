ffd48431e2f2e07b054013bd99c5f2159df5f432
tomrez12345 <126824722+tomrez12345@users.noreply.github.com>
2023-03-06 17:25:25 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -35,6 +35,8 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # pridani tabulky
 streamlit.dataframe(fruityvice_normalized)
 
+streamlit.stop ()
+
 import snowflake.connector
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
