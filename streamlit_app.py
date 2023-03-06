479f3207d0c039c311da0e26914a67b9237ab95e
tomrez12345 <126824722+tomrez12345@users.noreply.github.com>
2023-03-06 16:01:16 -0500

    Update streamlit_app.py
    pridani navrhu

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,6 +24,8 @@ streamlit.dataframe (fruits_to_show)
 
 # new section
 streamlit.header("Fruityvice Fruit Advice!")
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
 
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
@@ -32,3 +34,6 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # pridani tabulky
 streamlit.dataframe(fruityvice_normalized)
+
+
+
